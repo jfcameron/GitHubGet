333945bceab801c8bdf4d9f97c3f3185370ab4e0
Joseph Cameron <jf.cameron@gmail.com>
2018-07-18 23:35:59 -0400

    wip

--- src/main/java/io/github/jfcameron/githubget/SettingsCommand.java
+++ src/main/java/io/github/jfcameron/githubget/SettingsCommand.java
@@ -11,27 +11,25 @@ public class SettingsCommand extends Command
 {
     public SettingsCommand()
     {
-        super("settings", "interact with config on disk");
+        super("settings", "interact with config on disk.\n"
+                + "--check prints whether or not a token is present\n"
+                + "--token=[VALUE] sets token to value");
     }
 
     @Override
     protected void usermain(Parameter.List aParameters)
     {
         if (aParameters.containsLongOption("check"))
-        {
             if (Application.config.containsKey("GithubToken"))
                 System.out.println(Application.config.get("GithubToken"));
             else
                 System.out.println("No token in config");
-        }
         else
         {
             String token = aParameters.getLongOptionWithValue("token");
 
             if (token != null)
-            {
                 Application.config.setProperty("GithubToken", token);
-            }
         }
     }
 }
